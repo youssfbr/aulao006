8ea71e6152a7f3770542220653a08aff3a82fa5e
Alisson Youssf <youssfbr@gmail.com>
2023-11-06 00:38:15 -0300

    Migrate Spring projet, setup Postgres

--- src/main/java/com/github/youssfbr/aulajpamaven/dominio/Pessoa.java
+++ src/main/java/com/github/youssfbr/aulajpamaven/dominio/Pessoa.java
@@ -1,6 +1,14 @@
-package dominio;
+package com.github.youssfbr.aulajpamaven.dominio;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_pessoa")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
